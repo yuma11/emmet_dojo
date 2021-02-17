0b5b4118369698c40b672b80598790b1deb98767
yuma11 <11yuma20@gmail.com>
2021-02-17 15:48:11 +0900

    feat: トップページにカードレイアウトを追加

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    var lists = [
+      _modePanel("レベル１", "html-5", "blue", context),
+      _modePanel("レベル２", "html-5", "yellow", context),
+      _modePanel("レベル３", "html-5", "red", context),
+      _modePanel("ランダム", "html-5", "green", context),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(16, 0, 16, 32),
+          child: Column(
+            children: lists,
+          )),
+    );
+  }
+
+  // カードウィジェット
+  //---------------------------------------
+  Widget _modePanel(
+      String modeName, String modeImg, String color, BuildContext context) {
+    var _modeName = modeName;
+    var _modeImg = "assets/images/" + modeImg + ".svg";
+    var _modeColor;
+
+    switch (color) {
+      case 'blue':
+        _modeColor = Color(0xFF5c6bbf);
+        break;
+      case 'yellow':
+        _modeColor = Color(0xFFffa726);
+        break;
+      case 'red':
+        _modeColor = Color(0xFFef5350);
+        break;
+      case 'green':
+        _modeColor = Colors.teal[600];
+        break;
+    }
+
+    return InkWell(
+      onTap: () {
+        // Todo
+      },
+      child: Card(
+        margin: EdgeInsets.only(top: 32),
+        child: Container(
+          height: 100,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: _modeColor,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Column(children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(32),
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      _modeName,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      '達成率：10/10',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
